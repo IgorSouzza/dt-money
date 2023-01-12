000fb669525e42270031cdb9e7244b8b417523d1
Igor Souza <igor.souza.webmaster@gmail.com>
2023-01-12 17:59:15 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/globals"
+
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
