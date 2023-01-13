7daf1ad38f7577954183e4e64519f7665d4ab0e9
Igor Souza <igor.souza.webmaster@gmail.com>
2023-01-13 19:59:50 -0300

    feat: add table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,17 @@
+import * as S from './styles'
+
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { TransactionsTable } from './components/Table'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <S.TransactionsTableContainer>
+        <TransactionsTable />
+      </S.TransactionsTableContainer>
     </div>
   )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const TransactionsTableContainer = styled.div`
+  margin: 4rem 0;
+`
